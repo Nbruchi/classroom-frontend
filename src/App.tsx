1dd512fde1216c780b812e086078bc4eaa719f21
Nbruchi <nkundabagenzibruce@gmail.com>
2026-02-23 13:41:46 +0200

    feat: add faculty show page with departments and subjects tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { Authenticated, Refine } from "@refinedev/core";
+import { DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import routerProvider, {
     DocumentTitleHandler,
+    NavigateToResource,
     UnsavedChangesNotifier,
 } from "@refinedev/react-router";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
@@ -11,14 +12,35 @@ import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
-import Dashboard from "./pages/dashboard";
-import { Layout } from "./components/refine-ui/layout/layout";
-import { BookOpen, GraduationCap, Home, Users } from "lucide-react";
+import {
+    BookOpen,
+    Building2,
+    ClipboardCheck,
+    GraduationCap,
+    Home,
+    Users,
+} from "lucide-react";
 import SubjectsList from "./pages/subjects/list";
+import { Layout } from "./components/refine-ui/layout/layout";
 import SubjectsCreate from "./pages/subjects/create";
+import SubjectsShow from "./pages/subjects/show";
+import Dashboard from "./pages/dashboard";
+
+import { dataProvider } from "./providers/data";
 import ClassesList from "./pages/classes/list";
 import ClassesCreate from "./pages/classes/create";
+import ClassesShow from "./pages/classes/show";
+import { authProvider } from "./providers/auth";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import DepartmentsList from "./pages/departments/list";
+import DepartmentsCreate from "./pages/departments/create";
+import DepartmentShow from "./pages/departments/show";
+import FacultyList from "./pages/faculty/list";
+import FacultyShow from "./pages/faculty/show";
+import EnrollmentsCreate from "./pages/enrollments/create";
+import EnrollmentsJoin from "./pages/enrollments/join";
+import EnrollmentConfirm from "./pages/enrollments/confirm";
 
 function App() {
     return (
@@ -28,34 +50,41 @@ function App() {
                     <DevtoolsProvider>
                         <Refine
                             dataProvider={dataProvider}
+                            authProvider={authProvider}
                             notificationProvider={useNotificationProvider()}
                             routerProvider={routerProvider}
                             options={{
                                 syncWithLocation: true,
                                 warnWhenUnsavedChanges: true,
+                                projectId: "kkWuv7-GgBIfw-P8CGy0",
                             }}
                             resources={[
                                 {
                                     name: "dashboard",
                                     list: "/",
-                                    meta: { label: "Home", icon: <Home /> },
+                                    meta: {
+                                        label: "Home",
+                                        icon: <Home />,
+                                    },
                                 },
                                 {
                                     name: "subjects",
                                     list: "/subjects",
                                     create: "/subjects/create",
+                                    show: "/subjects/show/:id",
                                     meta: {
                                         label: "Subjects",
                                         icon: <BookOpen />,
                                     },
                                 },
                                 {
-                                    name: "classes",
-                                    list: "/classes",
-                                    create: "/classes/create",
+                                    name: "departments",
+                                    list: "/departments",
+                                    show: "/departments/show/:id",
+                                    create: "/departments/create",
                                     meta: {
-                                        label: "Classes",
-                                        icon: <GraduationCap />,
+                                        label: "Departments",
+                                        icon: <Building2 />,
                                     },
                                 },
                                 {
@@ -65,6 +94,15 @@ function App() {
                                     meta: {
                                         label: "Faculty",
                                         icon: <Users />,
+                                    },
+                                },
+                                {
+                                    name: "enrollments",
+                                    list: "/enrollments/create",
+                                    create: "/enrollments/create",
+                                    meta: {
+                                        label: "Enrollments",
+                                        icon: <ClipboardCheck />,
                                     },
                                 },
                                 {
@@ -82,12 +120,35 @@ function App() {
                             <Routes>
                                 <Route
                                     element={
-                                        <Layout>
-                                            <Outlet />
-                                        </Layout>
+                                        <Authenticated
+                                            key="public-routes"
+                                            fallback={<Outlet />}
+                                        >
+                                            <NavigateToResource fallbackTo="/" />
+                                        </Authenticated>
+                                    }
+                                >
+                                    <Route path="/login" element={<Login />} />
+                                    <Route
+                                        path="/register"
+                                        element={<Register />}
+                                    />
+                                </Route>
+
+                                <Route
+                                    element={
+                                        <Authenticated
+                                            key="private-routes"
+                                            fallback={<Login />}
+                                        >
+                                            <Layout>
+                                                <Outlet />
+                                            </Layout>
+                                        </Authenticated>
                                     }
                                 >
                                     <Route path="/" element={<Dashboard />} />
+
                                     <Route path="subjects">
                                         <Route
                                             index
@@ -97,7 +158,53 @@ function App() {
                                             path="create"
                                             element={<SubjectsCreate />}
                                         />
+                                        <Route
+                                            path="show/:id"
+                                            element={<SubjectsShow />}
+                                        />
                                     </Route>
+
+                                    <Route path="departments">
+                                        <Route
+                                            index
+                                            element={<DepartmentsList />}
+                                        />
+                                        <Route
+                                            path="create"
+                                            element={<DepartmentsCreate />}
+                                        />
+                                        <Route
+                                            path="show/:id"
+                                            element={<DepartmentShow />}
+                                        />
+                                    </Route>
+
+                                    <Route path="faculty">
+                                        <Route
+                                            index
+                                            element={<FacultyList />}
+                                        />
+                                        <Route
+                                            path="show/:id"
+                                            element={<FacultyShow />}
+                                        />
+                                    </Route>
+
+                                    <Route path="enrollments">
+                                        <Route
+                                            path="create"
+                                            element={<EnrollmentsCreate />}
+                                        />
+                                        <Route
+                                            path="join"
+                                            element={<EnrollmentsJoin />}
+                                        />
+                                        <Route
+                                            path="confirm"
+                                            element={<EnrollmentConfirm />}
+                                        />
+                                    </Route>
+
                                     <Route path="classes">
                                         <Route
                                             index
@@ -107,15 +214,19 @@ function App() {
                                             path="create"
                                             element={<ClassesCreate />}
                                         />
+                                        <Route
+                                            path="show/:id"
+                                            element={<ClassesShow />}
+                                        />
                                     </Route>
                                 </Route>
                             </Routes>
+
                             <Toaster />
                             <RefineKbar />
                             <UnsavedChangesNotifier />
                             <DocumentTitleHandler />
                         </Refine>
-                        <DevtoolsPanel />
                     </DevtoolsProvider>
                 </ThemeProvider>
             </RefineKbarProvider>
